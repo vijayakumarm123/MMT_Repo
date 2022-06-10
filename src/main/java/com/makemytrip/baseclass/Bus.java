c62a3922df0fd7f17435c92f92017307146f8217
vijaykumarm123 <vijaykumars700@gmail.com>
2022-06-10 12:39:36 +0530

    Tc 1,2,3,4 Updated

---
+++ src/main/java/com/makemytrip/baseclass/Bus.java
@@ -0,0 +1,15 @@
+package com.makemytrip.baseclass;
+
+public class Bus {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test case 1 created");
+		System.out.println("Test case 2 created");
+
+		System.out.println("Test case 3 created");
+		System.out.println("Test case 4 created");
+
+	}
+
+}
