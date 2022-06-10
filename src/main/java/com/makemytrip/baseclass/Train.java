d520b45ac63d1f436fd14c3688571e06e621617a
vijaykumarm123 <vijaykumars700@gmail.com>
2022-06-10 12:41:57 +0530

    Train TC 5 completed

---
+++ src/main/java/com/makemytrip/baseclass/Train.java
@@ -0,0 +1,18 @@
+package com.makemytrip.baseclass;
+
+public class Train {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test case 1 created");
+		System.out.println("Test case 2 created");
+
+		System.out.println("Test case 3 created");
+
+		System.out.println("Test case 4 created");
+		System.out.println("Test case 5 created");
+
+
+	}
+
+}
